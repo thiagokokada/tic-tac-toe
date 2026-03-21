a22ea3e88088ddf3775ada7c9de19d42967aaa41
Thiago Kenji Okada <thiagokokada@gmail.com>
2026-03-21 23:16:36 +0000

    tests/domain: add some more tests for Game dataclass

--- tests/domain/test_game.py
+++ tests/domain/test_game.py
@@ -1,8 +1,69 @@
 import random
 
+import pytest
+
 from app.domain.board import Board
+from app.domain.exceptions import GameFinishedError
 from app.domain.game import Game, make_computer_move
 from app.domain.schemas import Cell
+
+
+def test_game_defaults_to_empty_in_progress_state() -> None:
+    game = Game()
+
+    assert game.status.value == "in_progress"
+    assert game.moves == []
+    assert game.board.data == [
+        [Cell.NEUTRAL, Cell.NEUTRAL, Cell.NEUTRAL],
+        [Cell.NEUTRAL, Cell.NEUTRAL, Cell.NEUTRAL],
+        [Cell.NEUTRAL, Cell.NEUTRAL, Cell.NEUTRAL],
+    ]
+
+
+def test_apply_player_move_updates_board_moves_and_status() -> None:
+    game = Game()
+
+    game.apply_player_move(1, 1)
+
+    assert game.board.data == [
+        [Cell.NEUTRAL, Cell.NEUTRAL, Cell.NEUTRAL],
+        [Cell.NEUTRAL, Cell.X, Cell.NEUTRAL],
+        [Cell.NEUTRAL, Cell.NEUTRAL, Cell.NEUTRAL],
+    ]
+    assert len(game.moves) == 1
+    assert game.moves[0].player == Cell.X
+    assert (game.moves[0].x, game.moves[0].y) == (1, 1)
+    assert game.status.value == "in_progress"
+
+
+def test_move_history_is_kept_in_chronological_order() -> None:
+    game = Game(
+        computer_move_fn=lambda board: make_computer_move(board, rng=random.Random(0))
+    )
+
+    game.apply_player_move(1, 1)
+    game.apply_computer_move()
+
+    assert [(move.player, move.x, move.y) for move in game.moves] == [
+        (Cell.X, 1, 1),
+        (Cell.O, 1, 2),
+    ]
+
+
+def test_ensure_in_progress_raises_when_game_is_finished() -> None:
+    game = Game(
+        board=Board(
+            [
+                [Cell.X, Cell.X, Cell.X],
+                [Cell.NEUTRAL, Cell.O, Cell.NEUTRAL],
+                [Cell.O, Cell.NEUTRAL, Cell.O],
+            ]
+        )
+    )
+    game.update_status()
+
+    with pytest.raises(GameFinishedError, match="Game is already finished"):
+        game.ensure_in_progress()
 
 
 def test_apply_computer_move_places_o_in_available_position() -> None:
